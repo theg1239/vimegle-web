3b09f3911b5a25d776e1a0fd4283ab0d355cf24a
theg <52027622+theg1239@users.noreply.github.com>
2024-11-21 22:45:00 +0530

    YES

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,10 +1,26 @@
-import { io } from 'socket.io-client';
+import { io, Socket } from 'socket.io-client';
 
-const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3001';
+const SOCKET_URL = 'https://socket-vomegle-3829a22c3f54.herokuapp.com/';
 
-const socket = io(`${SERVER_URL}/text`, {
-  autoConnect: false, 
-  withCredentials: true,
+const socket: Socket = io(SOCKET_URL, {
+  transports: ['websocket'],
+  upgrade: false,
+  reconnection: true,
+  reconnectionAttempts: 5,
+  reconnectionDelay: 1000,
+  reconnectionDelayMax: 5000,
+});
+
+socket.on('ping', () => {
+  socket.emit('pong');
+});
+
+socket.on('reconnect_attempt', () => {
+  console.log('Attempting to reconnect to the server...');
+});
+
+socket.on('reconnect_failed', () => {
+  console.error('Reconnection attempts failed.');
 });
 
 export default socket;
