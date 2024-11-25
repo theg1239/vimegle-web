404ca0e204ba6a21187fe99aba0ef8b6f4cdea07
theg <52027622+theg1239@users.noreply.github.com>
2024-11-26 01:02:44 +0530

    chore: profanity

--- lib/profanity.ts
+++ lib/profanity.ts
@@ -4,7 +4,21 @@ const profaneWords = [
   'send nudes',
   'm or f',
   'f or m',
-  'bobs or vagana'
+  'bobs or vagana',
+  'm/f',
+  'f/m',
+  'Morf',
+  'M/F',
+  'bobs or vegana',
+  'G or B',
+  'B or G',
+  'boy or girl',
+  'boy or gurl',
+  'man or woman',
+  'man or girl',
+  'woman or man',
+  'm/f/',
+  'f/m/',
 ];
 
 const profaneRegex = new RegExp(
