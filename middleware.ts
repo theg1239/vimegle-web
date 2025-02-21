64850aa5f93e92c7f8245c34e5d618d8be8b2845
theg <52027622+theg1239@users.noreply.github.com>
2025-02-22 00:05:45 +0530

    maybe fix middleware

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,18 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { geolocation } from '@vercel/functions';
 
-export function middleware(request: NextRequest) {
-  const response = NextResponse.next();
-  const { country, city } = request.geo || {};
-  if (country) {
-    response.headers.set('x-partner-country', country);
-  }
-  if (city) {
-    response.headers.set('x-partner-city', city);
-  }
-  console.log(`Geo info for request: ${city ?? 'unknown'}, ${country ?? 'unknown'}`);
-  return response;
-}
+export const runtime = 'experimental-edge';
 
 export const config = {
   matcher: '/:path*',
 };
+
+export default function middleware(request: NextRequest) {
+  const response = NextResponse.next();
+  const { country = 'unknown', city = 'unknown' } = geolocation(request) || {};
+  response.headers.set('x-partner-country', country);
+  response.headers.set('x-partner-city', city);
+  console.log(`Geo info for request: ${city}, ${country}`);
+  return response;
+}
